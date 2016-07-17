2203e4114e9aecbb262d8ceae48328f3de007b5e
hachem2001 <abdeltif16@gmail.com>
2016-07-17 16:49:47 +0100

    This will be a lot better :dancer:

--- main.js
+++ main.js
@@ -2,12 +2,39 @@ window.onload = function() {
 	var canvas = document.getElementById("canvas"),
 		context = canvas.getContext("2d"),
 		width = canvas.width = window.innerWidth,
-		height = canvas.height = window.innerHeight;
+		height = canvas.height = window.innerHeight,
+		dx=0, dy=0;
 
-	for(var i=0;i<100;i++){
-		context.beginPath();
-		context.moveTo(Math.random()*width, Math.random()*height)
-		context.lineTo(Math.random()*width, Math.random()*height)
-		context.stroke();
+
+	s = 0
+	render();
+	function getDistance(x, y, x2, y2){
+		return Math.pow((x-x2)*(x-x2)+(y-y2)*(y-y2), 1/2)
 	}
+
+	function render(){
+		context.clearRect(0,0,width,height)
+		for(var h=0;h<height;h+=250){
+			for(var x=0;x<width;x+=17){
+				var y = h+Math.sin(s)*50
+				var ang = Math.atan2(x-dx, y-dy)
+
+				var intensity = 200/getDistance(x, y, dx, dy)
+				var ny = h+Math.sin(s-ang)*50*intensity
+				var nx = x+Math.cos(-ang)*50*intensity
+				context.beginPath();
+				context.arc(nx, ny, 50, 0, Math.PI*2, false)
+				context.fill();
+				context.stroke();
+				s=(s+0.0001)
+			}
+		}
+		requestAnimationFrame(render);
+	}
+	document.body.addEventListener("mousemove", function(event) {
+		dx = event.clientX ;
+		dy = event.clientY ;
+		angle = Math.atan2(dy, dx);
+	});
+
 };
